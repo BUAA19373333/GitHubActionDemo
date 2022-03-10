54e6146cacba0579b349462f37456e6f6c5756c7
BUAA19373333 <66054541+BUAA19373333@users.noreply.github.com>
2022-03-10 21:28:43 +0800

    put code

---
+++ main.py
@@ -0,0 +1,29 @@
+def perm(n, left, right):
+    global q
+    if left >= right:
+        q += n
+    else:
+        i = left
+        for j in range(left, right):
+            n[j], n[i] = n[i], n[j]
+            perm(n, left + 1, right)
+            n[j], n[i] = n[i], n[j]
+
+
+n = int(input())
+q = []
+a = []
+for i in range(1, n + 1):
+    a.append(i)
+perm(a, 0, n)
+b = []
+temp = 1
+for w in range(1, n + 1):
+    temp *= w
+for j in range(0, temp):
+    b.append(q[j * n:j * n + n])
+s = sorted(b)
+for r in s:
+    for c in r:
+        print(c, end=' ')
+    print()
